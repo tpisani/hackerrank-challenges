d39080d982c8b3b1cae011a8db6f6e0ab6de8934
Thiago Pisani <pisani.thiago@gmail.com>
2016-04-14 23:29:51 -0300

    Solution for Catity Map challenge

--- cavity_map/cavity_map.py
+++ cavity_map/cavity_map.py
@@ -4,7 +4,24 @@
 
 
 def cavities(M):
-    return []
+    n = len(M)
+    c = []
+
+    i = 1
+    j = 1
+
+    while i < n - 1:
+        k = M[i][j]
+
+        if k > M[i-1][j] and k > M[i+1][j] and k > M[i][j-1] and k > M[i][j+1]:
+            c.append([i, j])
+
+        j += 1
+        if j == n - 1:
+            i += 1
+            j = 1
+
+    return c
 
 
 if __name__ == "__main__":
--- cavity_map/test_cavity_map.py
+++ cavity_map/test_cavity_map.py
@@ -6,11 +6,23 @@ from . import cavity_map
 
 class CavityMapTests(TestCase):
 
-    def test_cavities_should_return(self):
+    def test_cavities_1(self):
         C = cavity_map.cavities([[1, 1, 1, 2],
                                  [1, 9, 1, 2],
                                  [1, 8, 9, 2],
                                  [1, 2, 3, 4]])
         self.assertEqual([[1, 1],
                           [2, 2]],
+                         C)
+
+    def test_cavities_2(self):
+        C = cavity_map.cavities([[0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 9, 0, 0],
+                                 [0, 0, 0, 0, 0, 0],
+                                 [0, 0, 9, 9, 0, 0],
+                                 [0, 9, 0, 0, 9, 0],
+                                 [0, 0, 0, 9, 0, 0]])
+        self.assertEqual([[1, 3],
+                          [4, 1],
+                          [4, 4]],
                          C)
